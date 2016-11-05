eff3930e7745b86967a1a30d304293b83826ef6f
Fraser Beaton <xdb14182@uni.strath.ac.uk>
2016-11-05 22:52:50 +0000

    added code to seperate js file and done most of road-crossing.html

---
+++ src/js/loadJson.js
@@ -0,0 +1,9 @@
+function jsoncallback(jsonObject){
+     for (var i = 0; i < jsonObject.Questions.length; i++) {
+        document.getElementById("question").innerHTML = jsonObject.Questions[i].question;
+        document.getElementById("ans1").innerHTML = jsonObject.Questions[i].answer1;
+        if(jsonObject.Questions[i].answer2 != undefined){
+          document.getElementById("ans2").innerHTML = jsonObject.Questions[i].answer2;
+        }
+      }
+    }
