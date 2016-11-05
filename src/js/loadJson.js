f27be3a9fad1a464218d725df8c0d9eccf7d59a4
WJohnM18 <williamacdonald18@googlemail.com>
2016-11-05 23:31:41 +0000

    Added working dynamic image functionality #MakeCodeGreatAgain

--- src/js/loadJson.js
+++ src/js/loadJson.js
@@ -1,6 +1,6 @@
 function jsoncallback(jsonObject){
      for (var i = 0; i < jsonObject.Questions.length; i++) {
-        document.getElementById("scenario-img").src = jsonObject.Questions[i].img;
+        document.getElementById("question-img").src = jsonObject.Questions[i].image;
         document.getElementById("question").innerHTML = jsonObject.Questions[i].question;
         document.getElementById("ans1").innerHTML = jsonObject.Questions[i].answer1;
         if(jsonObject.Questions[i].answer2 != undefined){
