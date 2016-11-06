0428c55eac18387bfa9d20534333c47b06a24462
WJohnM18 <williamacdonald18@googlemail.com>
2016-11-06 02:24:32 +0000

    Added main control flow logic and score counter

---
+++ src/js/gameControl.js
@@ -0,0 +1,56 @@
+var wrongAnswers = 0;
+let timeMultiplier = 5;
+var questionNum = 0;
+var questions;
+
+
+function jsoncallback(jsonObject){
+        questions = jsonObject.Questions;
+
+        update();  
+}
+
+function update(){
+    document.getElementById("question-img").src = questions[questionNum].image;
+    document.getElementById("question").innerHTML = questions[questionNum].question;  
+    document.getElementById("ans1").innerHTML = questions[questionNum].answer1;
+    document.getElementById("response1").innerHTML = questions[questionNum].response1;
+    if(questions[questionNum].answer2 != undefined){
+        document.getElementById("ans2").innerHTML = questions[questionNum].answer2;
+        document.getElementById("response2").innerHTML = questions[questionNum].response2;
+    }
+}
+
+function rightAnswer(ele){
+     
+    questionNum += 1;  
+    document.getElementById("ans1").style.display = 'none';
+    document.getElementById("ans2").style.display = 'none';
+ 	displayResponseBox(ele);       
+    update();
+}
+
+function wrongAnswer(eleId){
+     wrongAnswers += 1;
+     window.alert(wrongAnswers);
+     displayResponseBox(eleId);
+}
+
+function displayResponseBox(eleId){
+     if("ans1" === eleId){
+         $("#responseTile").show();
+         $("#responseTile2").hide();  
+     }
+     else if("ans2" === eleId){
+         $("#responseTile").hide();
+         $("#responseTile2").show();
+     }
+
+}
+
+function gameOver(){
+    return false;
+}
+
+
+
