bb42161fc5e5e31002426dc156608f6a160dfbc7
Fraser Beaton <xdb14182@uni.strath.ac.uk>
2016-11-06 00:25:30 +0000

    fixing merge

--- src/js/loadJson.js
+++ src/js/loadJson.js
@@ -2,8 +2,10 @@ function jsoncallback(jsonObject){
      for (var i = 0; i < jsonObject.Questions.length; i++) {
         document.getElementById("question").innerHTML = jsonObject.Questions[i].question;
         document.getElementById("ans1").innerHTML = jsonObject.Questions[i].answer1;
+        document.getElementById("response1").innerHTML = jsonObject.Questions[i].response2;
         if(jsonObject.Questions[i].answer2 != undefined){
           document.getElementById("ans2").innerHTML = jsonObject.Questions[i].answer2;
+          document.getElementById("response2").innerHTML = jsonObject.Questions[i].response1;
         }
       }
     }
