d599755de0b31e66784f83511e672a16aa7c1546
Fraser Beaton <xdb14182@uni.strath.ac.uk>
2016-11-06 04:17:17 +0000

    added a summary page for now with a new method in the gamecontrol js file

--- src/js/gameControl.js
+++ src/js/gameControl.js
@@ -5,14 +5,13 @@ var questions;
 
 
 function jsoncallback(jsonObject){
-        questions = jsonObject.Questions;
-
-        update();  
+    questions = jsonObject.Questions;
+    update();
 }
 
 function update(){
     document.getElementById("question-img").src = questions[questionNum].image;
-    document.getElementById("question").innerHTML = questions[questionNum].question;  
+    document.getElementById("question").innerHTML = questions[questionNum].question;
     document.getElementById("ans1").innerHTML = questions[questionNum].answer1;
     document.getElementById("response1").innerHTML = questions[questionNum].response1;
     if(questions[questionNum].answer2 != undefined){
@@ -22,35 +21,41 @@ function update(){
 }
 
 function rightAnswer(ele){
-     
-    questionNum += 1;  
+    questionNum += 1;
     document.getElementById("ans1").style.display = 'none';
     document.getElementById("ans2").style.display = 'none';
- 	displayResponseBox(ele);       
+ 	  displayResponseBox(ele);
     update();
 }
 
 function wrongAnswer(eleId){
      wrongAnswers += 1;
-     window.alert(wrongAnswers);
+     //window.alert(wrongAnswers);
      displayResponseBox(eleId);
+}
+
+function getTime() {
+  // var timeLate = wrongAnswers * timeMultiplier;
+  var timeLate = 25;
+  if(timeLate != 0){
+    document.getElementById("hidden").innerHTML = "Well done you were on time !";
+  }else{
+    document.getElementById("hidden").innerHTML = "Oh no ! you were " + timeLate + " minutes late !";
+    document.getElementById("hidden2").innerHTML = "Try again and plan a safer route !";
+  }
 }
 
 function displayResponseBox(eleId){
      if("ans1" === eleId){
          $("#responseTile").show();
-         $("#responseTile2").hide();  
+         $("#responseTile2").hide();
      }
      else if("ans2" === eleId){
          $("#responseTile").hide();
          $("#responseTile2").show();
      }
-
 }
 
 function gameOver(){
     return false;
 }
-
-
-
